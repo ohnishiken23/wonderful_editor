8e035ebda69c42da47526fc00d1c267f7c32ade9
大西健 <ohnishiken5@gmail.com>
2024-03-22 23:45:01 +0900

    render修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('NodePolyfillPlugin', new NodePolyfillPlugin())
+
 module.exports = environment
