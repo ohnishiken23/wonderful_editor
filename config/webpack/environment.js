ffbd7c62dc89abda9bf73718f44db2237637e968
大西健 <ohnishiken5@gmail.com>
2024-03-24 00:16:20 +0900

    render修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 
-const vueRule = environment.loaders.get('vue')
-vueRule.test = /\.vue$/
-vueRule.use[0].options.extractCSS = false // Add this line to disable CSS extraction
+// const vueRule = environment.loaders.get('vue')
+// vueRule.test = /\.vue$/
+// vueRule.use[0].options.extractCSS = false // Add this line to disable CSS extraction
 
 module.exports = environment
